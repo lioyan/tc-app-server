4115efe00ab1e802533aa70c31cebf26ad934884
liuyan <meetliuyan@gmail.com>
2020-09-08 11:39:42 +0800

    fix: add timestamp

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,13 +3,13 @@ import axios from 'axios';
 @Injectable()
 export class AppService {
   async getHello(): Promise<string> {
-    const url = 'https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1';
+    const url = `https://cn.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&t=${new Date().getTime()}`;
     const { data } = await axios.get(url);
-    return `https://cn.bing.com${data.images[0].url}`
+    return `https://cn.bing.com${data.images[0].url}`;
   }
   async getHitokoto(): Promise<string> {
     const url = 'https://v1.hitokoto.cn/';
     const { data } = await axios.get(url);
-    return data
+    return data;
   }
 }
